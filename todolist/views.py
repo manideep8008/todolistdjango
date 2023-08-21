13646293027c84d455966a3b8b4ba85f3a7b81e0
manideep8008 <manideepreddyboddu@gmail.com>
2023-08-21 22:11:19 +0530

    second commit

--- todolist/views.py
+++ todolist/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import HttpResponse,render
+from todo.models import Task
 
 def home(request):
-    return render(request,"home.html")
+    taskscom=Task.objects.filter(is_completed=False).order_by('-updatedAt')
+    context={
+        'taskscom':taskscom
+    }
+    # tasksinc=Task.objects.filter(is_completed=True)
+    # context={
+    #     "tasksinc":tasksinc
+    # }
+    return render(request,"home.html",context)
